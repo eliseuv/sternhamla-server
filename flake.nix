60f61912438a1099549468a50f9f7e5668f5442d
evf <eliseuv816@gmail.com>
2025-08-10 18:34:38 -0300

    Development environment flake

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "Rust";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      fenix,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ self.overlays.default ];
+      };
+    in
+    {
+
+      overlays.default = final: prev: {
+        rustToolchain =
+          with fenix.packages.${prev.stdenv.hostPlatform.system};
+          combine (
+            with stable;
+            [
+              rustc
+              cargo
+              rust-src
+              rustfmt
+              clippy
+            ]
+          );
+      };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+
+        packages = with pkgs; [
+
+          rustToolchain
+
+          rust-analyzer
+          bacon
+
+          # Cargo tools
+          cargo-watch
+          cargo-cross
+          cargo-fuzz
+          cargo-nextest
+          cargo-deny
+          cargo-edit
+
+          # Compilation cache
+          sccache
+
+          # Debugging
+          lldb
+
+          # https://nixos.wiki/wiki/Rust#Building_Rust_crates_that_require_external_system_libraries
+          openssl.dev
+          pkg-config
+
+        ];
+
+        env = {
+          RUST_BACKTRACE = 1;
+          RUST_LOG = "warn";
+          # Compilation cache
+          RUSTC_WRAPPER = "sccache";
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          # OpenSSL config
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+
+        shellHook = ''
+          cargo --version
+        '';
+
+      };
+
+    };
+
+}
