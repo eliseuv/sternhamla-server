ee8988bf84688bb2f3255ef8d2b7210899734f93
evf <eliseuv816@gmail.com>
2025-08-12 15:29:27 -0300

    Remove other games

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 //! Sternhalma game server
 //!
 
-/// Simple Tic-Tac-Toe implementation for reference
-pub mod tictactoe;
-
 /// Starhalma game representation
 pub mod sterhalma;
-
-/// Game of 4 criteria
-pub mod four_criteria;
