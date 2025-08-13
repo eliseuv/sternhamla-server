1fe9da431da9f4bae3e4372b04a9c46a173ae32e
evf <eliseuv816@gmail.com>
2025-08-13 10:14:07 -0300

    Binary for experimenting with the game

--- src/bin/test-sternhalma.rs
+++ src/bin/test-sternhalma.rs
@@ -1,27 +1,82 @@
-use std::collections::HashMap;
-
-use rand::{Rng, SeedableRng, seq::IteratorRandom};
-use rand_xoshiro::Xoshiro256PlusPlus;
+use itertools::Itertools;
+use rand::seq::{IndexedRandom, IteratorRandom};
+use rand_distr::{Distribution, Poisson};
+use rand_xoshiro::{Xoshiro256PlusPlus, rand_core::SeedableRng};
 
 use sternhalma_server::sterhalma::{
     Game, GameStatus,
-    board::{Board, movement::MovementFull, player::Player},
+    board::{Board, HexIdx, hex_distance, lut, movement::Movement, player::Player},
 };
 
-struct Agent {
-    player: Player,
+trait Agent {
+    /// Create new agent for given player
+    fn new(player: Player) -> Self
+    where
+        Self: Sized;
+
+    /// Select a movement based on the current state of the board
+    fn select_movement(&mut self, board: &Board<Player>) -> Movement;
 }
 
-impl Agent {
+struct AgentBrownian {
+    player: Player,
+    rng: Xoshiro256PlusPlus,
+}
+
+impl Agent for AgentBrownian {
     fn new(player: Player) -> Self {
-        Self { player }
+        Self {
+            player,
+            rng: Xoshiro256PlusPlus::from_os_rng(),
+        }
     }
 
-    fn select_movement(&mut self, board: &Board<Player>, rng: &mut impl Rng) -> MovementFull {
+    fn select_movement(&mut self, board: &Board<Player>) -> Movement {
         board
             .iter_player_movements(&self.player)
-            .choose(rng)
-            .expect("No available movements")
+            .map(|mv| mv.get_indices())
+            .choose(&mut self.rng)
+            .unwrap()
+    }
+}
+
+struct AgentMin {
+    player: Player,
+    rng: Xoshiro256PlusPlus,
+    dist: Poisson<f32>,
+    goal: [HexIdx; 15],
+}
+
+impl Agent for AgentMin {
+    fn new(player: Player) -> Self {
+        let goal = match player {
+            Player::Player1 => lut::PLAYER2_STARTING_POSITIONS,
+            Player::Player2 => lut::PLAYER1_STARTING_POSITIONS,
+        };
+        Self {
+            player,
+            rng: Xoshiro256PlusPlus::from_os_rng(),
+            dist: Poisson::new(3.0).unwrap(),
+            goal,
+        }
+    }
+
+    fn select_movement(&mut self, board: &Board<Player>) -> Movement {
+        let movements = board
+            .iter_player_movements(&self.player)
+            .map(|mv| mv.get_indices())
+            .collect::<Vec<_>>();
+        let mv_min = movements
+            .iter()
+            .filter(|mv| !self.goal.contains(&mv.from))
+            .sorted_unstable_by_key(|mv| {
+                self.goal.iter().map(|idx| hex_distance(mv.to, *idx)).min()
+            })
+            .nth(self.dist.sample(&mut self.rng) as usize);
+        match mv_min {
+            Some(mv) => mv.clone(),
+            None => movements.choose(&mut self.rng).unwrap().clone(),
+        }
     }
 }
 
@@ -31,21 +86,19 @@ fn main() {
     let mut game = Game::new();
     println!("{game}");
 
-    let mut agents = HashMap::from([
-        (Player::Player1, Agent::new(Player::Player1)),
-        (Player::Player2, Agent::new(Player::Player2)),
-    ]);
+    let mut agent1 = AgentMin::new(Player::Player1);
+    let mut agent2 = AgentBrownian::new(Player::Player2);
 
-    let mut rng = Xoshiro256PlusPlus::from_os_rng();
+    while let GameStatus::Playing { player, turns } = game.status()
+        && turns < 1024
+    {
+        let agent: &mut dyn Agent = match player {
+            Player::Player1 => &mut agent1,
+            Player::Player2 => &mut agent2,
+        };
 
-    while let GameStatus::Playing(player) = game.status() {
-        game.unsafe_apply_movement(
-            &agents
-                .get_mut(&player)
-                .unwrap()
-                .select_movement(game.board(), &mut rng)
-                .get_indices(),
-        );
+        let movement = agent.select_movement(game.board());
+        game.unsafe_apply_movement(&movement);
         println!("{game}");
     }
 }
