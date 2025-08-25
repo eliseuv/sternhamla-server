dcd1506bd2f5a2997a14f95ec6d8198fb3e58d29
evf <eliseuv816@gmail.com>
2025-08-25 14:22:17 -0300

    [Flake] add cache sources

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "Rust";
 
+  nixConfig = {
+
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
+  };
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     fenix = {
